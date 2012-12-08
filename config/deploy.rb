4ad78fe27b7e143bdad47a8b8b26d049b39a1b07
Pete Birkinshaw <pete@binary-ape.org>
2012-12-08 17:22:28 +0000

    New deployment config

---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+deployment_user = ENV['DEPUSER']
+raise "No DEPUSER!" unless deployment_user
+
+set :application, "bao_website"
+set :deploy_to, "/srv/www/vhosts/bao"
+set :deploy_via, :copy
+set :repository, "htdocs" 
+set :scm, :none 
+set :copy_compression, :gzip
+set :use_sudo, false
+
+set :user, deployment_user
+
+role :web, 'binary-ape.org'
